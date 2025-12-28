729f67f365674b1367ed2556bd2bf44299f06c8d
Agoth Arop <agoth.a3@gmail.com>
2025-12-27 23:07:32 -0700

    increased scaffolding and header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-
+import { Header } from './components/Header'
+import { useState } from 'react'
 function App() {
-
+const [score, setScore] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <main className='h-screen bg-linear-to-t from-sky-500 to-gray-950'>
+        <Header score={score}/>
+
+    </main>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export function Header({ score }) {
+	return (
+		<header className="text-white px-4 py-4 flex items-center flex-col md:flex-row md:justify-between md:px-8 gap-4">
+			<h1 className="font-bold text-4xl ">Memory Card App</h1>
+			<div className="flex gap-6">
+				<h3 className="font-bold text-xl">Score:</h3>
+				<p className="text-xl">{score}</p>
+			</div>
+		</header>
+	);
+}
