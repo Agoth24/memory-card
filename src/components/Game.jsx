4ebe8a2f42da56d8f94bad1de4745eaa4784adad
Agoth Arop <agoth.a3@gmail.com>
2025-12-28 22:57:51 -0700

    logic comments and removed title shadow

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,6 +6,7 @@ function Game({ setScore }) {
 	const [cards, setCards] = useState(() => people);
 	const [prevIds, setPrevIds] = useState([]);
 
+    // Fisher-Yates Shuffle implementation
 	function shuffleCards() {
 		const shuffled = [...cards];
 		for (let i = shuffled.length - 1; i > 0; i--) {
@@ -19,6 +20,7 @@ function Game({ setScore }) {
 		return !prevIds.includes(id1);
 	}
 
+    // Increments or resets score based on game logic
 	function handleCardClick(card) {
 		const id = card.id;
 		if (!checkValidCard(id)) {
